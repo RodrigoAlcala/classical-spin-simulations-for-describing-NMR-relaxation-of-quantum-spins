58735dd4cfffa79f84e5e59dee217f455c7f5395
RodrigoAlcala <rodrigoalcala00@yahoo.com>
2021-11-09 11:06:21 -0300

    Delete iteratedSMean.m

--- iteratedSMean.m
+++
@@ -1,5 +0,0 @@
-function [SMean] = iteratedSMean(iter=500)    % Autor: Rodrigo N. Alcalá M.    % Licencia: General Public License V3    % Simulación FID en CaF_2, sistema 5x5        SIter = [];    SMean = [];    iterPoints = [];        for i = 1:iter        S = spinGenerator5x5();        SIter = cat(3,SIter,S);
-    endfor
-        for n = 1:5        for m = 1:3            iterPoints = SIter(m,n,:);            SMean(m,n) = (mean(iterPoints));
-        endfor
-    endfor
